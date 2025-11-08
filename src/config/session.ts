a51a17f72395b7b9f53d7e373caf18cf879432e9
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-08 05:42:36 +0500

    feat: add session middleware with pg as session store

---
+++ src/config/session.ts
@@ -0,0 +1,21 @@
+import session from "express-session";
+import connectPgSimple from "connect-pg-simple";
+import pool from "./database.js";
+
+const PGStore = connectPgSimple(session);
+const isProduction = process.env.NODE_ENV == "production";
+
+export default session({
+  secret: process.env.SESSION_SECRET || "secret",
+  store: new PGStore({
+    pool: pool,
+    createTableIfMissing: true,
+  }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: isProduction,
+    secure: isProduction,
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+  },
+});
