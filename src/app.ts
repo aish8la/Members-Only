c5397591a1bdfdb63655169355e1c3b526f4334d
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-09 01:20:49 +0500

    format: apply formate to database.ts and app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
 const app = express();
 
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(`Server listening on port ${PORT}`);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`Server listening on port ${PORT}`);
 });
