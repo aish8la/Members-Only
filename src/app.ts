3d444b034c73312a089b2506f8ed8fe5b78e32eb
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-12 04:17:19 +0500

    feat: setup passport,  urlencoded and ejs middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import "./config/envConfig.js";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 import express from "express";
+import passport from "passport";
+import session from "./config/session.js";
+import { localStrategy } from "./config/passportConfig.js";
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+passport.use(localStrategy);
+app.use(session);
+app.use(passport.session());
 
 const PORT = Number(process.env.PORT) || 3000;
 
