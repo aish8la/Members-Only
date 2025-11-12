e39763000de855bc250a234c3cd1a38a4fa979bb
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-13 00:40:49 +0500

    feat: implement route and controller to sign up

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,5 +1,23 @@
-import type express from "express";
+import express from "express";
+import * as db from "../db/users.js";
+import * as argon2 from "argon2";
 
 export const getSignUp: express.RequestHandler = (req, res) => {
   res.render("authentication/signup");
+};
+
+export const postSignUp: express.RequestHandler = async (req, res, next) => {
+  const { rowCount } = await db.addUser({
+    emailAddress: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: await argon2.hash(req.body.password),
+    isMember: false,
+    isAdmin: false,
+  });
+  if (rowCount !== 1) {
+    const err = new Error("Something Unexpected happened");
+    return next(err);
+  }
+  res.redirect("/log-in");
 };
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -2,6 +2,6 @@ import express from "express";
 import * as controller from "../controllers/signup.js";
 const router = express.Router();
 
-router.route("/").get(controller.getSignUp);
+router.route("/").get(controller.getSignUp).post(controller.postSignUp);
 
 export default router;
