15ef97dc4492fdd60da3afa9f97d50a8bdd2bfd6
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-15 00:12:54 +0500

    feat: checks for authentication on root route with checkAuthStatus middleware

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -2,9 +2,6 @@ import type express from "express";
 
 export const getHome: express.RequestHandler = (req, res) => {
   const authenticated = req.isAuthenticated();
-  if (!authenticated) {
-    return res.redirect("/auth/login");
-  }
   res.render("index", {
     isAuthenticated: authenticated,
     currentUser: req.user,
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import * as controller from "../controllers/root.js";
+import { checkAuthStatus } from "../middleware/auth.js";
 const router = express.Router();
 
-router.route("/").get(controller.getHome);
+router.route("/").get(checkAuthStatus(), controller.getHome);
 
 export default router;
