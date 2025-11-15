00b8ab11ddb8e91330a568a9797ad721f40e74e4
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-15 06:17:57 +0500

    feat: add validation to sign up route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import * as controller from "../controllers/auth.js";
 import * as middleware from "../middleware/auth.js";
+import { setUpValidator } from "../middleware/validate.js";
+import { signUpValidation } from "../validators/validationChains.js";
 const router = express.Router();
 
 router.route("/login").get(controller.getLogin).post(middleware.authenticate);
-router.route("/signup").get(controller.getSignup).post(controller.postSignup);
+router
+  .route("/signup")
+  .get(controller.getSignup)
+  .post(setUpValidator(signUpValidation), controller.postSignup);
 router.route("/logout").post(controller.postLogout);
 
 export default router;
