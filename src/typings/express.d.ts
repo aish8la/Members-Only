a39e5c0271d5f7e31692b9162c587dfe43fdeab9
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-15 21:26:02 +0500

    chore: change typing of validatedData object to unknown

--- src/typings/express.d.ts
+++ src/typings/express.d.ts
@@ -4,7 +4,7 @@ declare global {
   namespace Express {
     interface User extends SafeUser {}
     interface Request {
-      validatedData?: Record<string, unknown>;
+      validatedData?: unknown;
     }
   }
 }
