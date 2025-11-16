ba71946c1991fe9160a8bbb0d623019c216caace
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-16 22:32:35 +0500

    feat: use validation on user join post route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import { postUserJoin } from "../controllers/user.js";
+import { setUpValidator } from "../middleware/validate.js";
+import { joinClubValidation } from "../validators/validationChains.js";
 const router = express.Router();
 
-router.route("/:userId/join").post(postUserJoin);
+router
+  .route("/:userId/join")
+  .post(setUpValidator(joinClubValidation, "/"), postUserJoin);
 
 export default router;
