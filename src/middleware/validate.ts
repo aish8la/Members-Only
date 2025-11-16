675750b6ceeb3a6f2ed30feab5fefd2d0dca2aca
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-16 21:53:47 +0500

    feat: extend validation middleware to accept a function in place of a redirect string

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -5,9 +5,17 @@ import {
   type ValidationChain,
 } from "express-validator";
 
+/**
+ * Run the Validation chains in the provided array and store validated data and errors in
+ * session, before redirecting to specified path or running the given function
+ *
+ * @param { ValidationChain[] } schema - An array of validation chains to run
+ * @param { string | RequestHandler } errorRedirect - Either a string representing the redirect path or middleware function to run in case of validation error
+ *
+ */
 export const setUpValidator = (
   schema: ValidationChain[],
-  errorRedirect: string
+  errorRedirect: string | RequestHandler
 ) => {
   const validationMiddleware: RequestHandler = async (req, res, next) => {
     await Promise.all(schema.map((v) => v.run(req)));
@@ -22,6 +30,14 @@ export const setUpValidator = (
     }
     req.session.formErrors = errors.mapped();
     req.session.formData = validatedData;
+
+    if (typeof errorRedirect === "function") {
+      return req.session.save((err) => {
+        if (err) return next(err);
+        return errorRedirect(req, res, next);
+      });
+    }
+
     return req.session.save((err) => {
       if (err) return next(err);
       res.redirect(errorRedirect);
