b2197b2ad1994c158bd37f2ca13685941e419bb2
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-22 00:11:20 +0500

    feat: add approximate time post was created using date-fns library

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -1,12 +1,15 @@
 import type express from "express";
+import { formatDistanceToNowStrict } from "date-fns";
 import { getAllMessages } from "../db/message.js";
 
 export const getHome: express.RequestHandler = async (req, res) => {
   const authenticated = req.isAuthenticated();
   const messages = await getAllMessages();
+
   res.render("index", {
     isAuthenticated: authenticated,
     messages: messages,
+    formatDistanceToNowStrict: formatDistanceToNowStrict,
     currentUser: req.user,
   });
 };
