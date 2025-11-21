6bf7794ff6c52ea23b897c1df4c48ebebd500a47
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-21 23:36:36 +0500

    feat: add message EJS template to display messages and refactor route to display messages

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -1,9 +1,12 @@
 import type express from "express";
+import { getAllMessages } from "../db/message.js";
 
-export const getHome: express.RequestHandler = (req, res) => {
+export const getHome: express.RequestHandler = async (req, res) => {
   const authenticated = req.isAuthenticated();
+  const messages = await getAllMessages();
   res.render("index", {
     isAuthenticated: authenticated,
+    messages: messages,
     currentUser: req.user,
   });
 };
