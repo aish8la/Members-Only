beccb1566a47d48f68469fbc3de48ebdb0806f7f
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-11-23 22:47:45 +0500

    feat: add style to header and toast partial

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ const __dirname = path.dirname(__filename);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: false }));
 passport.use(localStrategy);
